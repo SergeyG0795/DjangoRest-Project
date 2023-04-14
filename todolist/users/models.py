c9abee4fffb58af7f5188170bb1032a04e6f313e
sergey <svg95@list.ru>
2023-04-14 22:23:26 +0400

    lesson_1

--- todolist/users/models.py
+++ todolist/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthday = models.DateField()
+    email = models.CharField(max_length=100)
---
+++ todolist/users/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.viewsets import ModelViewSet
+
+from users.models import User
+
+
+class UserModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+
+
